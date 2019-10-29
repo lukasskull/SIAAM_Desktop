420e4db544250180540d5168f61209dd6362e28c
Lukas Putra Diantama <lukasputra33@gmail.com>
2019-10-29 13:08:41 +0700

    Init

---
+++ src/views/FromPemesanantoFromDetailPemesanan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views;
+
+import content.Data_pemesanan;
+
+/**
+ *
+ * @author LUKAS PUTRA DIANTAMA
+ */
+public class FromPemesanantoFromDetailPemesanan {
+    public static void main (String[] args){
+        Data_pemesanan data_pemesanan = new Data_pemesanan();
+        data_pemesanan.setVisible(true);
+    }
+}
---
+++ src/views/passinggeneratedkey.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views;
+
+import content.Data_detail_pemesanan;
+
+/**
+ *
+ * @author LUKAS PUTRA DIANTAMA
+ */
+public class passinggeneratedkey {
+    public static void main (String[] args){
+        Data_detail_pemesanan data_pemesanan = new Data_detail_pemesanan();
+        data_pemesanan.setVisible(true);
+    }
+}
